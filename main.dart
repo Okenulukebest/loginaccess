62ed511070db26fb7a89c316a577f2deb640be98
Lukebest Okenu <Lokenu@cihpng.org>
2024-08-01 20:17:44 +0100

    loginaccess

---
+++ main.dart
@@ -0,0 +1,14 @@
+void main(){
+  var username = 'Lukebest';
+  var password = 'Admin123';
+  int age = 18;
+  if(username == 'Lukebest'){
+    if(password == 'Admin123'){
+      if(age == 18){
+        print('Login Successfully');
+      }else{
+        print('Access Denied');
+      }
+    }
+  }
+}
