6d12c2e01f38c640516947c733b3b9f511890124
Lukebest Okenu <Lokenu@cihpng.org>
2024-08-03 11:58:30 +0100

    loginaccess

--- main.dart
+++ main.dart
@@ -1,10 +1,28 @@
 void main(){
   var username = 'Lukebest';
   var password = 'Admin123';
-  int age = 18;
+  int age = 14;
+  userauthenticator(username, password, age);
+}
+void userauthenticator(var username, var password, int age){
+  if(username == 'Lukebest' && password == 'Admin123' && age >= 18){
+        print('Login Successfully');
+      }
+      if(username == 'Lukebest' && password != 'Admin123' && age >= 18){
+        print('Note: Ensure you are 18years and above. \n Error: Username or Password supplied is wrong. Do you want to reset your passord?');
+      }
+      if(username == 'Lukebest' && password == 'Admin123' && age < 18){
+        print('Error: Age not within the minimum requirement');
+      }
+      else{
+        print('Access Denied');
+      }
+    }
+
+void userauthenticator2(var username, var password, int age){
   if(username == 'Lukebest'){
     if(password == 'Admin123'){
-      if(age == 18){
+      if(age >= 18){
         print('Login Successfully');
       }else{
         print('Access Denied');
